50c2f8940202fb51328794000006fb99c1d76d37
Philip Tuffs <philipmtuffs@gmail.com>
2022-08-12 11:34:50 -0700

    tag table

--- src/components/MetricModal.tsx
+++ src/components/MetricModal.tsx
@@ -17,14 +17,69 @@ interface State {
 
 }
 
+interface TagDataFrame {
+    name: string;
+    fields: {
+        name: string;
+        type: FieldType;
+        values: any[]
+    }[]
+}
+
+interface TagGridMap {
+    [key: string]: any[]
+}
+
 export default class MetricModal extends Component<Props, State> {
+
+    ref: React.RefObject<HTMLDivElement>;
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+
+        }
+        this.ref = React.createRef();
+    }
+
+    buildTagGrid = (tags: MetricImage["tags"]): TagDataFrame => {
+        let tagGridMap: TagGridMap = {}
+        for (let tag of tags) {
+            for (let point in tag) {
+                if (!tagGridMap[point]) tagGridMap[point] = [];
+                tagGridMap[point].push(String(tag[point]))
+            }
+        }
+
+
+        let dataFrameBuild = {
+            name: '',
+            fields: [{
+                name: 'Tag',
+                type: FieldType.string,
+                values: tagGridMap[Object.keys(tagGridMap)[0]].map((tag, i) => {
+                    return String(i);
+                })
+
+            }, ...Object.keys(tagGridMap).map(tag => {
+                return {name: tag.charAt(0).toUpperCase() + tag.slice(1), type: FieldType.string, values: tagGridMap[tag]}
+            })],
+        }
+
+        return dataFrameBuild;
+
+    }
 
     render = () => {
 
         if (!this.props.figure || !this.props.image) return null;
 
+        let tags = this.buildTagGrid(this.props.image.tags);
+
+        let modalWidth = this.ref.current?.getBoundingClientRect().width ?? 200;
+
         return <GrafanaUI.Modal isOpen={this.props.isOpen} title="Metric Details" onDismiss={this.props.onDismiss} >
-            <div style={{ display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: "100%" }} >
+            <div ref={this.ref} style={{ display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: "100%", }} >
                 {!this.props.figure && <img src={this.props.image?.img} style={{ width: 400, height: 400, borderRadius: 10, opacity: 0.2 }} />}
                 {!this.props.figure && <div style={{ position: 'absolute' }} >
                     <GrafanaUI.LoadingPlaceholder  />
@@ -34,13 +89,10 @@ export default class MetricModal extends Component<Props, State> {
                     <p style={{ fontSize: 24}} >Metric</p>
                     <p style={{ marginLeft: 20 }} >{this.props.image?.metric}</p>
                     <p style={{ fontSize: 24}} >Tags</p>
-                    <GrafanaUI.Table width={200} height={200} data={toDataFrame({
-                        name: 'foo bar',
-                        fields: [
-                            { name: 'Tag', type: FieldType.string, values: ["one", "two"] },
-                            { name: 'Value', type: FieldType.number, values: [1, 2] },
-                        ],
-                    })} />
+                    <div style={{ overflow: 'scroll', width:modalWidth }} >
+                        <GrafanaUI.Table width={modalWidth} height={400} data={toDataFrame(tags)} />
+                    </div>
+                    
                 </div>
             </div>
         </GrafanaUI.Modal>
