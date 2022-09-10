95607510667002dc74597e712f5e144d1e6b81d1
Philip Tuffs <philipmtuffs@gmail.com>
2022-09-10 16:20:34 -0700

    update dir, impl cp method

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require("path");
 const fs = require("fs");
+const exec = require('child_process').exec;
 
 module.exports = (config, options) => {
     config.module.rules.push({
         test: /\.svg/,
         type: 'asset',
     })
-    let buildPath = fs.readFileSync("./buildPath.txt", 'utf-8');
-    config.output.path = path.resolve(buildPath + '/anomalizer-grafana-plugin');
+
+    config.plugins.push({
+        apply: (compiler) => {
+            compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                let buildPath = fs.readFileSync("./grafanaPluginDirectory.txt", 'utf-8');
+                exec(`cp -r dist/ ${buildPath}/pogadog-anomalizer-panel`, (err, stdout, stderr) => {
+                    process.stdout.write(`  Plugin successfully copied to ${buildPath}\n`);
+                });
+            });
+        }
+      })
+
     return config;
 }
