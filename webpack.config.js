71bfe2eebd785027c32f2012df300442147bdbe3
Philip Tuffs <philipmtuffs@gmail.com>
2022-09-10 16:45:04 -0700

    comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const fs = require("fs");
 const exec = require('child_process').exec;
 
 module.exports = (config, options) => {
+
+    // correctly include svgs as base64 assets
     config.module.rules.push({
         test: /\.svg/,
         type: 'asset',
     })
 
+    // copy built plugin to shared Grafana Docker plugin directory
     config.plugins.push({
         apply: (compiler) => {
             compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
