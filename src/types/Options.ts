0a355eca9189b383abe0b30cbfd10a35c2bf6541
Philip Tuffs <philipmtuffs@gmail.com>
2022-10-05 16:28:29 -0700

    remove options

--- src/types/Options.ts
+++ src/types/Options.ts
@@ -1,17 +1,5 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
+
 
 export default interface OptionsTypes {
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
-  metricType: "timeseries" | "scatter";
-  refreshRate: string;
-  primaryUIFilter: string;
-  primaryUIFilterInvert: 'match' | 'notMatch';
-  primaryServerFilter: string;
-  primaryServerFilterInvert: 'match' | 'notMatch';
-  secondaryUIFilter: string;
-  secondaryUIFilterInvert: 'match' | 'notMatch';
-  secondaryServerFilter: string;
-  secondaryServerFilterInvert: 'match' | 'notMatch';
-  metricWeightPreference: "alpha" | "spike" | "rstd" | "max" | "rmax" | "mean";
+
 }
